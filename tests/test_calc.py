7bea9789b74cb47d9984313dab49610d1d5db6df
Alex Markin <alexndr86@gmail.com>
2021-07-22 11:01:35 +0300

    Add exception testing example

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -21,8 +21,14 @@ class TestCalc(unittest.TestCase):
 
     def test_divide(self):
         self.assertEqual(calc.divide(10, 5), 2)
+        self.assertEqual(calc.divide(5, 2), 2.5)
         self.assertEqual(calc.divide(-1, 1), -1)
         self.assertEqual(calc.divide(-1, -1), 1)
+
+        # self.assertRaises(ValueError, calc.divide, 10, 0)
+
+        with self.assertRaises(ValueError):
+            calc.divide(10, 0)
 
 
 if __name__ == "__main__":
